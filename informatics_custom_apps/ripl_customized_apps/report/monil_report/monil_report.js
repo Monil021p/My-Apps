2d78fba720d42bc8db64b390eee00ac2eb3982c9
Monil Kamboj <monilkamboj021@gmail.com>
2025-05-07 20:43:42 +0530

    Report Updation

--- informatics_custom_apps/ripl_customized_apps/report/monil_report/monil_report.js
+++ informatics_custom_apps/ripl_customized_apps/report/monil_report/monil_report.js
@@ -1,52 +1,5 @@
 // Copyright (c) 2025, Monil Kamboj and contributors
 // For license information, please see license.txt
-
-// frappe.query_reports["Monil Report"] = {
-// 	"filters": [
-// 		{
-// 			fieldname: "plant",
-// 			label: "Plant",
-// 			fieldtype: "Link",
-// 			options: "Branch",
-// 			reqd: 1
-// 		},
-// 		{
-// 			fieldname: "from_date",
-// 			label: "From Date",
-// 			fieldtype: "Date",
-// 			reqd: 1
-// 		},
-// 		{
-// 			fieldname: "to_date",
-// 			label: "To Date",
-// 			fieldtype: "Date",
-// 			reqd: 1
-// 		},
-// 		{
-// 			fieldname: "gl_entry",
-// 			label: "GL Entry",
-// 			fieldtype: "Link",
-// 			options: "GL Entry",
-// 			reqd: 1,
-// 			get_query: function (doc) {
-// 				const plant = frappe.query_report.get_filter_value("plant");
-// 				const from_date = frappe.query_report.get_filter_value("from_date");
-// 				const to_date = frappe.query_report.get_filter_value("to_date");
-		
-// 				return {
-// 					filters: [
-// 						["GL Entry", "branch", "=", plant],
-// 						["GL Entry", "posting_date", ">=", from_date],
-// 						["GL Entry", "posting_date", "<=", to_date],
-// 						["GL Entry", "voucher_type", "IN",["Sales Invoice","Purchase Invoice"]	]
-// 					]
-// 				};
-// 			}
-// 		}
-		
-// 	]
-// };
-
 frappe.query_reports["Monil Report"] = {
     "filters": [
         {
@@ -69,59 +22,24 @@ frappe.query_reports["Monil Report"] = {
             reqd: 1
         },
         {
-            fieldname: "gl_entry",
-            label: "GL Entry",
+            fieldname: "account",
+            label: "Account",
             fieldtype: "Link",
-            options: "GL Entry",
             reqd: 1,
-            get_query: function (doc) {
-                const plant = frappe.query_report.get_filter_value("plant");
-                const from_date = frappe.query_report.get_filter_value("from_date");
-                const to_date = frappe.query_report.get_filter_value("to_date");
-        
-                return {
-                    filters: [
-                        ["GL Entry", "branch", "=", plant],
-                        ["GL Entry", "posting_date", ">=", from_date],
-                        ["GL Entry", "posting_date", "<=", to_date],
-                        ["GL Entry", "voucher_type", "IN",["Sales Invoice","Purchase Invoice"]	]
-                    ]
-                };
-            }
+            options: "Account"
         }
-        // {
-        //     fieldname: "extra_field",
-        //     label: "Pick Additional Fields",
-        //     fieldtype: "Select",
-        //     options: [],  // filled dynamically
-        //     reqd: 0
-        // }        
-    ]
-//     onload: function(report) {
-//         frappe.call({
-//             method: "informatics_custom_apps.ripl_customized_apps.report.monil_report.monil_report.get_report_field_options",
-//             callback: function(r) {
-//                 if (r.message) {
-//                     frappe.show_alert({
-//                         message: __('Fields Fetched Successfully!'),
-//                         indicator: 'blue'
-//                     });
-    
-//                     let opts = [{ label: "", value: "" }];
-//                     (r.message || []).forEach(f => {
-//                         opts.push({
-//                             label: f.label,
-//                             value: f.value
-//                         });
-//                     });
-    
-//                     const extraField = frappe.query_report.get_filter("extra_field");
-//                     extraField.df.options = opts;
-//                     extraField.refresh();
-//                 }
-//             }
-//         });
-//     }
-    
+    ],
+
+    onload: function(report) {
+        // Wait for filters to fully render before clearing
+        setTimeout(() => {
+            ["plant", "from_date", "to_date", "account"].forEach(fieldname => {
+                let filter = frappe.query_report.get_filter(fieldname);
+                if (filter) {
+                    filter.set_value("");  // or `null`, but "" works better for Link fields
+                }
+            });
+        }, 100);
+    }
     
 };
--- informatics_custom_apps/ripl_customized_apps/report/monil_report/monil_report.py
+++ informatics_custom_apps/ripl_customized_apps/report/monil_report/monil_report.py
@@ -4,289 +4,111 @@
 import frappe
 
 def execute(filters=None):
-	if filters==None:
+	if filters is None:
 		return
 	columns = get_columns()
 	data = get_data(filters)
 	return columns, data
 
-
 def get_columns():
-	return[
-		{
-			"label":"Plant",
-			"fieldtype":"Data",
-			"fieldname":"plant",
-		},
-		{
-			"label":"Sales Invoice",
-			"fieldtype":"Link",
-			"fieldname":"s_inv",
-			"options":"Sales Invoice"
-		},
-		{
-			"label":"Purchase Invoice",
-			"fieldtype":"Link",
-			"fieldname":"p_inv",
-			"options":"Purchase Invoice"
-		},
-		{
-			"label":"Cost Center",
-			"fieldtype":"Data",
-			"fieldname":"cost_center",
-		},
-		{
-			"label":"Customer Code",
-			"fieldtype":"Link",
-			"fieldname":"customer",
-			"options":"Customer"
-		},
-		{
-			"label":"Customer Name",
-			"fieldtype":"Data",
-			"fieldname":"customer_name",
-		},
-		{
-			"label":"Supplier Code",
-			"fieldtype":"Link",
-			"fieldname":"supplier",
-			"options":"Supplier"
-		},
-		{
-			"label":"Supplier Name",
-			"fieldtype":"Data",
-			"fieldname":"supplier_name",
-		},
-		{
-			"label":"Date",
-			"fieldtype":"Date",
-			"fieldname":"posting_date",
-			"read_only":1
-		},
-		{
-			"label":"Segment",
-			"fieldtype":"Data",
-			"fieldname":"segment",
-		},
-		{
-			"label":"Item Group",
-			"fieldtype":"Data",
-			"fieldname":"custom_item_group",
-		},
-		{
-			"label":"Item Code",
-			"fieldtype":"Link",
-			"fieldname":"item",
-			"options":"Item"
-		},
-		{
-			"label":"Item Name",
-			"fieldtype":"Data",
-			"fieldname":"item_name",
-		},
-		{
-			"label":"Quantity",
-			"fieldtype":"Data",
-			"fieldname":"qty",
-		},
-		{
-			"label":"Rate",
-			"fieldtype":"Data",
-			"fieldname":"rate",
-		},
-		{
-			"label":"Amount",
-			"fieldtype":"Data",
-			"fieldname":"amount",
-		},
-		{
-			"label":"Total Tax",
-			"fieldtype":"Data",
-			"fieldname":"total_taxes_and_charges"
-		},
-		{
-			"label":"Grand Total",
-			"fieldtype":"Data",
-			"fieldname":"grand_total"
-		},
-		{
-			"label":"UOM",
-			"fieldtype":"Data",
-			"fieldname":"uom",
-		}
+	return [
+		{"label": "Plant", "fieldtype": "Data", "fieldname": "plant"},
+		{"label": "Debit", "fieldtype": "Data", "fieldname": "debit"},
+		{"label": "Credit", "fieldtype": "Data", "fieldname": "credit"},
+		{"label": "GL Entry", "fieldtype": "Link", "fieldname": "gl_entry", "options": "GL Entry"},
+		{"label": "Sales Invoice", "fieldtype": "Link", "fieldname": "s_inv", "options": "Sales Invoice"},
+		{"label": "Purchase Invoice", "fieldtype": "Link", "fieldname": "p_inv", "options": "Purchase Invoice"},
+		{"label": "Cost Center", "fieldtype": "Data", "fieldname": "cost_center"},
+		{"label": "Customer Code", "fieldtype": "Link", "fieldname": "customer", "options": "Customer"},
+		{"label": "Customer Name", "fieldtype": "Data", "fieldname": "customer_name"},
+		{"label": "Supplier Code", "fieldtype": "Link", "fieldname": "supplier", "options": "Supplier"},
+		{"label": "Supplier Name", "fieldtype": "Data", "fieldname": "supplier_name"},
+		{"label": "Date", "fieldtype": "Date", "fieldname": "posting_date", "read_only": 1},
+		{"label": "Segment", "fieldtype": "Data", "fieldname": "segment"},
+		{"label": "Item Group", "fieldtype": "Data", "fieldname": "custom_item_group"},
+		{"label": "Item Code", "fieldtype": "Link", "fieldname": "item", "options": "Item"},
+		{"label": "Item Name", "fieldtype": "Data", "fieldname": "item_name"},
+		{"label": "Quantity", "fieldtype": "Data", "fieldname": "qty"},
+		{"label": "Rate", "fieldtype": "Data", "fieldname": "rate"},
+		{"label": "Amount", "fieldtype": "Data", "fieldname": "amount"},
+		{"label": "Total Tax", "fieldtype": "Data", "fieldname": "total_taxes_and_charges"},
+		{"label": "Grand Total", "fieldtype": "Data", "fieldname": "grand_total"},
+		{"label": "UOM", "fieldtype": "Data", "fieldname": "uom"},
 	]
-# def get_data(filters=None):
-# 	output=[]
-# 	a = frappe.get_doc("Branch",filters.get("plant"))
-#   b = frappe.get_all("Sales Invoice",filters={'branch':a.name,'docstatus':1,'posting_date':['BETWEEN',[filters.from_date,filters.to_date]]})
-# 	if g.voucher_type == "Sales Invoice":
-# 		b = frappe.get_doc("Sales Invoice",{"name":g.voucher_no})
-# 		print("------------->Sales Invoice:",b)
-# 		for i in b:
-# 			sales_invoice = frappe.get_doc("Sales Invoice",i.name)
-# 			for item in sales_invoice.items:
-# 				item_code=item.item_code
-# 				item_name = item.item_name
-# 				qty=item.qty
-# 				rate=item.rate
-# 				amount=item.amount
-# 				uom=item.uom
-# 			si = {
-# 				'plant':a.name,
-# 				's_inv':sales_invoice.name,
-# 				'cost_center':sales_invoice.cost_center,
-# 				'customer':sales_invoice.customer,
-# 				'customer_name':sales_invoice.customer_name,
-# 				'posting_date':sales_invoice.posting_date,
-# 				'segment': sales_invoice.segment,
-# 				'custom_item_group': sales_invoice.custom_item_group,
-# 				'item': item_code,
-# 				'item_name':item_name,
-# 				'qty':qty,
-# 				'rate':rate,
-# 				'amount':amount,
-# 				'uom':uom,
-# 				'total_taxes_and_charges':sales_invoice.total_taxes_and_charges,
-# 				'grand_total':sales_invoice.grand_total
-# 			}
-# 			print("---------------->",i)
-# 			output.append(si)
-# 		return output
-# 	else:
-# 		pass
 
 def get_data(filters=None):
-	output=[]
-	a = frappe.get_doc("Branch",filters.get("plant"))
-	try:
-		g = frappe.get_doc("GL Entry",filters.get("gl_entry"))
-	except Exception as e:
-		print("------Exception:",e)
-	# b = frappe.get_all("Sales Invoice",filters={'branch':a.name,'docstatus':1,'posting_date':['BETWEEN',[filters.from_date,filters.to_date]]})
-	if g.voucher_type == "Sales Invoice":
-		b = frappe.get_doc("Sales Invoice",{"name":g.voucher_no})
-		print("------------->Sales Invoice:",b)
+	output = []
 
-		sales_invoice = frappe.get_doc("Sales Invoice",b.name)
-		print("------------->Sales Invoice2:",sales_invoice)
-		for item in sales_invoice.items:
-			item_code=item.item_code
-			item_name = item.item_name
-			qty=item.qty
-			rate=item.rate
-			amount=item.amount
-			uom=item.uom
-		si = {
-			'plant':a.name,
-			's_inv':sales_invoice.name,
-			'cost_center':sales_invoice.cost_center,
-			'customer':sales_invoice.customer,
-			'customer_name':sales_invoice.customer_name,
-			'posting_date':sales_invoice.posting_date,
-			'segment': sales_invoice.segment,
-			'custom_item_group': sales_invoice.custom_item_group,
-			'item': item_code,
-			'item_name':item_name,
-			'qty':qty,
-			'rate':rate,
-			'amount':amount,
-			'uom':uom,
-			'total_taxes_and_charges':sales_invoice.total_taxes_and_charges,
-			'grand_total':sales_invoice.grand_total
-		}
-		# print("---------------->",i)
-		output.append(si)
-		return output
-	elif g.voucher_type == "Purchase Invoice":
-		b = frappe.get_doc("Purchase Invoice",{"name":g.voucher_no})
-		print("------------->Puhasrcg Invoice:",b)
+	# Get Plant and Account
+	plant = frappe.get_doc("Branch", filters.get("plant"))
+	account = frappe.get_doc("Account", filters.get("account"))
 
-		purchase_invoice = frappe.get_doc("Purchase Invoice",b.name)
-		print("------------->Purchase Invoice2:",purchase_invoice)
-		for item in purchase_invoice.items:
-			item_code=item.item_code
-			item_name = item.item_name
-			qty=item.qty
-			rate=item.rate
-			amount=item.amount
-			uom=item.uom
-		pi = {
-			'plant':a.name,
-			'p_inv':purchase_invoice.name,
-			'cost_center':purchase_invoice.cost_center,
-			'supplier':purchase_invoice.supplier,
-			'supplier_name':purchase_invoice.supplier_name,
-			'posting_date':purchase_invoice.posting_date,
-			'segment': purchase_invoice.segment,
-			'item': item_code,
-			'item_name':item_name,
-			'qty':qty,
-			'rate':rate,
-			'amount':amount,
-			'uom':uom,
-			'total_taxes_and_charges':purchase_invoice.total_taxes_and_charges,
-			'grand_total':purchase_invoice.grand_total
-		}
-		# print("---------------->",i)
-		output.append(pi)
-		return output
-	
-# @frappe.whitelist()
-# def get_report_field_options():
-#     field_sources = {
-#         "Sales Invoice": "Sales Invoice",
-#         "Purchase Invoice": "Purchase Invoice",
-#         "Sales Invoice Item": "Sales Invoice Item",
-#         "Purchase Invoice Item": "Purchase Invoice Item"
-#     }
+	# Get GL Entries
+	gl_entries = frappe.get_all(
+    "GL Entry",
+    filters={
+        "branch": plant.name,
+        "docstatus": 1,
+        "account": account.name,
+        "posting_date": ["between", [filters.get("from_date"), filters.get("to_date")]]
+    },
+    fields=["name", "voucher_type", "voucher_no"], 
+    limit_page_length=10000 
+)
 
-#     options = []
+	print("--------------------------------------->GL:",gl_entries)
+	print("----------------->Number of GL Entries found:", len(gl_entries))
+	for gl in gl_entries:
+		if gl.voucher_type == "Sales Invoice":
+			gle=frappe.get_doc("GL Entry", gl.name)
+			sales_invoice = frappe.get_doc("Sales Invoice", gl.voucher_no)
+			print("------------------->salesinvoice",sales_invoice)
+			for item in sales_invoice.items:
+				output.append({
+					"plant": plant.name,
+					"s_inv": sales_invoice.name,
+					"debit": gle.debit,
+					"credit":gle.credit,
+					"gl_entry":gle.name,
+					"cost_center": sales_invoice.cost_center,
+					"customer": sales_invoice.customer,
+					"customer_name": sales_invoice.customer_name,
+					"posting_date": sales_invoice.posting_date,
+					"segment": sales_invoice.segment,
+					"custom_item_group": sales_invoice.custom_item_group,
+					"item": item.item_code,
+					"item_name": item.item_name,
+					"qty": item.qty,
+					"rate": item.rate,
+					"amount": item.amount,
+					"uom": item.uom,
+					"total_taxes_and_charges": sales_invoice.total_taxes_and_charges,
+					"grand_total": sales_invoice.grand_total
+				})
 
-#     for doctype_label, doctype in field_sources.items():
-#         meta = frappe.get_meta(doctype)
-#         for df in meta.fields:
-#             if df.fieldtype not in ["Section Break", "Column Break", "Button", "HTML"] and df.fieldname:
-#                 label = f"{doctype_label} → {df.label or df.fieldname}"
-#                 options.append({
-#                     "value": f"{doctype}.{df.fieldname}",  # used internally
-#                     "label": label  # shown to user
-#                 })
-	
-#     return sorted(options, key=lambda x: x["label"])
+		elif gl.voucher_type == "Purchase Invoice":
+			purchase_invoice = frappe.get_doc("Purchase Invoice", gl.voucher_no)
+			gle = frappe.get_doc("GL Entry", gl.name)
+			for item in purchase_invoice.items:
+				output.append({
+					"plant": plant.name,
+					"p_inv": purchase_invoice.name,
+					"cost_center": purchase_invoice.cost_center,
+					"supplier": purchase_invoice.supplier,
+					"gl_entry":gle.name,
+					"supplier_name": purchase_invoice.supplier_name,
+					"posting_date": purchase_invoice.posting_date,
+					"segment": purchase_invoice.segment,
+					"item": item.item_code,
+					"item_name": item.item_name,
+					"debit": gle.debit,
+					"credit":gle.credit,
+					"qty": item.qty,
+					"rate": item.rate,
+					"amount": item.amount,
+					"uom": item.uom,
+					"total_taxes_and_charges": purchase_invoice.total_taxes_and_charges,
+					"grand_total": purchase_invoice.grand_total
+				})
 
-# def get_columns(extra_fields=None):
-#     base_columns = [
-#         {"label": "Plant", "fieldtype": "Data", "fieldname": "plant"},
-#         {"label": "Sales Invoice", "fieldtype": "Link", "fieldname": "s_inv", "options": "Sales Invoice"},
-#         {"label": "Purchase Invoice", "fieldtype": "Link", "fieldname": "p_inv", "options": "Purchase Invoice"},
-#         {"label": "Cost Center", "fieldtype": "Data", "fieldname": "cost_center"},
-#         {"label": "Customer Code", "fieldtype": "Link", "fieldname": "customer", "options": "Customer"},
-#         {"label": "Customer Name", "fieldtype": "Data", "fieldname": "customer_name"},
-#         {"label": "Supplier Code", "fieldtype": "Link", "fieldname": "supplier", "options": "Supplier"},
-#         {"label": "Supplier Name", "fieldtype": "Data", "fieldname": "supplier_name"},
-#         {"label": "Date", "fieldtype": "Date", "fieldname": "posting_date", "read_only": 1},
-#         {"label": "Segment", "fieldtype": "Data", "fieldname": "segment"},
-#         {"label": "Item Group", "fieldtype": "Data", "fieldname": "custom_item_group"},
-#         {"label": "Item Code", "fieldtype": "Link", "fieldname": "item", "options": "Item"},
-#         {"label": "Item Name", "fieldtype": "Data", "fieldname": "item_name"},
-#         {"label": "Quantity", "fieldtype": "Data", "fieldname": "qty"},
-#         {"label": "Rate", "fieldtype": "Data", "fieldname": "rate"},
-#         {"label": "Amount", "fieldtype": "Data", "fieldname": "amount"},
-#         {"label": "Total Tax", "fieldtype": "Data", "fieldname": "total_taxes_and_charges"},
-#         {"label": "Grand Total", "fieldtype": "Data", "fieldname": "grand_total"},
-#         {"label": "UOM", "fieldtype": "Data", "fieldname": "uom"},
-#     ]
-
-#     dynamic_columns = []
-#     for field in extra_fields or []:
-#         doctype, fieldname = field.split(".", 1)
-#         meta = frappe.get_meta(doctype)
-#         df = next((f for f in meta.fields if f.fieldname == fieldname), None)
-#         if df:
-#             dynamic_columns.append({
-#                 "label": f"{doctype} - {df.label or fieldname}",
-#                 "fieldname": field.replace(".", "_"),  # e.g. Sales Invoice.customer_name → sales_invoice_customer_name
-#                 "fieldtype": df.fieldtype or "Data",
-#             })
-
-#     return base_columns + dynamic_columns
+	return output
